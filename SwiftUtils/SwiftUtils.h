98f721ae635221c32127daed6c8283d31cb3f69d
Juan Rodriguez <juanrodriguezliebana@icloud.com>
2021-01-04 10:54:59 +0100

    Initial Commit

---
+++ SwiftUtils/SwiftUtils.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUtils.h
+//  SwiftUtils
+//
+//  Created by Juan Rodríguez on 4/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUtils.
+FOUNDATION_EXPORT double SwiftUtilsVersionNumber;
+
+//! Project version string for SwiftUtils.
+FOUNDATION_EXPORT const unsigned char SwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtils/PublicHeader.h>
+
+
